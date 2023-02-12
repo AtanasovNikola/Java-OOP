4581b86a204c0079cc2e20d3af3f31b1481a34aa
AtanasovNikola <atanasov_nikola99@abv.bg>
2023-02-13 01:58:18 +0200

    Practicing Test-drive development

---
+++ src/main/java/chainBlock/Transaction.java
@@ -0,0 +1,10 @@
+package chainBlock;
+
+public interface Transaction {
+    int getId();
+    TransactionStatus getStatus();
+    void setStatus(TransactionStatus status);
+    double getAmount();
+    String getFrom();
+}
+
